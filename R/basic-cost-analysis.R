b782625456335f079223316c53b7e0002fd1552e
David.Ellis <david.ellis@birmingham.gov.uk>
2025-02-04 16:42:12 +0000

    Added analysis of total cost and change by IMD and ethnic diversity

--- R/basic-cost-analysis.R
+++ R/basic-cost-analysis.R
@@ -12,33 +12,40 @@ library(ggplot2)
 maint_data <- read_excel(
   "data/park-work-records/maintenance-all-years.xlsx"
   ) %>%
-  group_by(
-    YearStart, Year
-  ) %>%
-  summarise(
-    Cost = sum(Cost)
-  ) %>%
   mutate(
     Type = "Maintenance"
+  ) %>%
+  select(
+    -Play_Park
   )
 
 ft_data <- read_excel(
   "data/park-work-records/fly-tipping-all-years.xlsx"
 ) %>%
-  group_by(
-    YearStart, Year
-  ) %>%
-  summarise(
-    Cost = sum(Cost)
-  ) %>%
   mutate(
     Type = "Fly Tipping"
   )
 
-# Combine maintenance and fly tipping data to plot together
-comb_work_data <- rbind(maint_data, ft_data)
+park_demog <- read_excel(
+  "output/park_demographics.xlsx"
+)
 
-ggplot(comb_work_data, aes(x = YearStart, y = Cost/1e6, color = Type)) +
+comb_work_data <- rbind(maint_data, ft_data) 
+
+##############################################################
+#                     Plot yearly totals                     #
+##############################################################
+
+# Combine maintenance and fly tipping data to plot together
+work_totals <- comb_work_data %>%
+  group_by(
+    Type, YearStart, Year
+  ) %>%
+  summarise(
+    Cost = sum(Cost)
+  ) 
+
+ggplot(work_totals, aes(x = YearStart, y = Cost/1e6, color = Type)) +
   geom_line() +
   geom_point() +
   theme_bw() +
@@ -54,3 +61,83 @@ ggplot(comb_work_data, aes(x = YearStart, y = Cost/1e6, color = Type)) +
     labels = paste(seq(13, 23, 2), seq(14, 24, 2), sep = "/")
     )
 
+##############################################################
+#                  Plot total cost vs IMD                    #
+##############################################################
+
+# Create data frame with 2013 cost for each park
+first_date <- comb_work_data %>%
+  filter(
+    YearStart == 2013
+  ) %>% 
+  group_by(Old_Site_Ref) %>%
+  summarise(
+    First_Cost = sum(Cost)
+  )
+
+# Create data frame with 2023 cost for each park
+last_date <- comb_work_data %>%
+  filter(
+    YearStart == 2023
+  ) %>% 
+  group_by(Old_Site_Ref) %>%
+  summarise(
+    Last_Cost = sum(Cost)
+  )
+
+# Create data frame with total cost, cost change, and demographic data
+# for each park
+park_totals_and_change <- comb_work_data %>%
+  group_by(
+    Old_Site_Ref
+  ) %>%
+  summarise(
+    Total_Cost = sum(Cost)
+  ) %>%
+  left_join(
+    first_date,
+    by = join_by("Old_Site_Ref")
+  ) %>%
+  left_join(
+    last_date,
+    by = join_by("Old_Site_Ref")
+  ) %>%
+  mutate(
+    Spend_Change = Last_Cost - First_Cost
+  ) %>%
+  left_join(
+    park_demog,
+    by = join_by("Old_Site_Ref")
+  ) %>%
+  mutate(
+    `Non-white %` = 100*(Total_Population - White)/Total_Population
+  )
+
+# Total cost (2013-2023) vs IMD rank
+ggplot(park_totals_and_change, 
+       aes(IMD_rank, Total_Cost)
+       ) +
+  geom_point() +
+  theme_bw()
+
+# Spend change (2013-2023) vs IMD rank
+ggplot(park_totals_and_change, 
+       aes(IMD_rank, Spend_Change)
+) +
+  geom_point() +
+  theme_bw()
+
+# Total cost (2013-2023) vs local Non-white population %
+ggplot(park_totals_and_change, 
+       aes(`Non-white %`, Total_Cost)
+) +
+  geom_point() +
+  theme_bw()
+
+
+# Spend change (2013-2023) vs local Non-white population %
+ggplot(park_totals_and_change, 
+       aes(`Non-white %`, Spend_Change)
+) +
+  geom_point() +
+  theme_bw()
